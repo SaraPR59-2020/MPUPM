0d4c72641accee6680f0f4461a5a918f9422c7ad
Sara Stamenkovic <stamenkovic.pr59.2020@uns.ac.rs>
2024-04-17 17:22:39 +0200

    App commit

---
+++ ModelLabsProjekat/ModelLabs/NetworkModelService/DataModel/Outage/SwitchingOperation.cs
@@ -0,0 +1,100 @@
+﻿using FTN.Common;
+using FTN.Services.NetworkModelService.DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTN.Services.NetworkModelService.DataModel.Outage
+{
+    public class SwitchingOperation : IdentifiedObject
+    {
+        public SwitchingOperation(long globalId) : base(globalId)
+        {
+        }
+
+        public SwichState NewState { get; set; }
+        public DateTime OperationTime { get; set; }
+        public long OutageSchedule { get; set; } = 0;
+
+        public override bool Equals(object obj)
+        {
+            if (base.Equals(obj))
+            {
+                SwitchingOperation so = (SwitchingOperation)obj;
+                return ((so.OutageSchedule == this.OutageSchedule) && (so.OperationTime == this.OperationTime) &&(so.NewState == this.NewState));
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        #region IAccess implementation
+
+        public override bool HasProperty(ModelCode property)
+        {
+            switch (property)
+            {
+                case ModelCode.SWITCHINGOPERATION_NEWSTATE:
+                case ModelCode.SWITCHINGOPERATION_OPERATIONTIME:
+                case ModelCode.SWITCHINGOPERATION_OUTAGESCHEDULE:
+                    return true;
+
+                default:
+                    return base.HasProperty(property);
+            }
+        }
+
+        public override void GetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.SWITCHINGOPERATION_NEWSTATE:
+                    property.SetValue((short)NewState);
+                    break;
+                case ModelCode.SWITCHINGOPERATION_OPERATIONTIME:
+                    property.SetValue(OperationTime);
+                    break;
+                case ModelCode.SWITCHINGOPERATION_OUTAGESCHEDULE:
+                    property.SetValue(OutageSchedule);
+                    break;
+
+                default:
+                    base.GetProperty(property);
+                    break;
+            }
+        }
+
+        public override void SetProperty(Property property)
+        {
+            switch (property.Id)
+            {
+                case ModelCode.SWITCHINGOPERATION_NEWSTATE:
+                    NewState = (SwichState)property.AsEnum();
+                    break;
+                case ModelCode.SWITCHINGOPERATION_OPERATIONTIME:
+                    OperationTime = property.AsDateTime();
+                    break;
+                case ModelCode.SWITCHINGOPERATION_OUTAGESCHEDULE:
+                    OutageSchedule = property.AsReference();
+                    break;
+                
+
+
+                default:
+                    base.SetProperty(property);
+                    break;
+            }
+        }
+
+        #endregion IAccess implementation
+
+    }
+}
---
+++ ModelLabsProjekat/ModelLabs/NetworkModelService/DataModel/Wires/Switch.cs
@@ -0,0 +1,113 @@
+﻿using FTN.Common;
+using FTN.Services.NetworkModelService.DataModel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTN.Services.NetworkModelService.DataModel.Wires
+{
+    public class Switch : ConductingEquipment
+    {
+        public Switch(long globalId) : base(globalId)
+        {
+        }
+        public bool NormalOpen { get; set; }
+        public bool Retained { get; set; }
+        public int SwitchOnCount { get; set; }
+        public DateTime SwitchOnDate { get; set; }
+
+
+        public override bool Equals(object x)
+        {
+            if (base.Equals(x))
+            {
+                Switch s = (Switch)x;
+                return ((s.NormalOpen == this.NormalOpen) && (s.Retained == this.Retained) && (s.SwitchOnCount == this.SwitchOnCount) && (s.SwitchOnDate == this.SwitchOnDate));
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        #region IAccess implementation
+
+        public override bool HasProperty(ModelCode property)
+        {
+            switch (property)
+            {
+                case ModelCode.SWITCH_NORMALOPEN:
+                case ModelCode.SWITCH_RETAINED:
+                case ModelCode.SWITCH_SWITCHONCOUNT:
+                case ModelCode.SWITCH_SWITCHONDATE:
+                    return true;
+
+                default:
+                    return base.HasProperty(property);
+            }
+        }
+
+        public override void GetProperty(Property prop)
+        {
+            switch(prop.Id)
+            {
+                case ModelCode.SWITCH_NORMALOPEN:
+                    prop.SetValue(NormalOpen);
+                    break;
+                case ModelCode.SWITCH_RETAINED:
+                    prop.SetValue(Retained);
+                    break;
+                case ModelCode.SWITCH_SWITCHONCOUNT:
+                    prop.SetValue(SwitchOnCount);
+                    break;
+                case ModelCode.SWITCH_SWITCHONDATE:
+                    prop.SetValue(SwitchOnDate);
+                    break;
+
+                default:
+                    base.GetProperty(prop);
+                    break;
+            }
+        }
+
+        public override void SetProperty(Property property)
+        {
+            switch(property.Id)
+            {
+                case ModelCode.SWITCH_NORMALOPEN:
+                    NormalOpen = property.AsBool();
+                    break;
+                case ModelCode.SWITCH_RETAINED:
+                    Retained = property.AsBool();
+                    break;
+                case ModelCode.SWITCH_SWITCHONCOUNT:
+                    SwitchOnCount = property.AsInt();
+                    break;
+                case ModelCode.SWITCH_SWITCHONDATE:
+                    SwitchOnDate = property.AsDateTime();
+                    break;
+
+                default:
+                    base.SetProperty(property);
+                    break;
+            }
+        }
+
+        #endregion IAccess implementation
+
+        #region IReference implementation
+
+        public override void GetReferences(Dictionary<ModelCode, List<long>> references, TypeOfReference refType)
+        {
+            base.GetReferences(references, refType);
+        }
+
+        #endregion IReference implementation
+    }
+}
